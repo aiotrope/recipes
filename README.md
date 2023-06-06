667885b2b9beea0d43ea67d15e1e83c49d29c010
aiotrope <arneli74@protonmail.com>
2023-06-06 12:39:57 +0300

    ♻️ refactor(app): init post and fetch single recipe request

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # recipes
-Weekly exercise 4.
+Fourth weekly exercise.
+
+## CLI Commands
+
+```bash
+# init node project with express generator
+$ express --view=ejs recipes
+
+# install dependencies
+$ npm install
+
+# run the code 
+$ npm run start
+
+# run on debug mode
+$ npm run dev
+
+```
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const Recipes = [];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Recipes", recipes: Recipes });
+});
+
+// Saving user
+router.post("/recipe/", (req, res, next) => {
+  let { name, instruction, ingredient } = req.body;
+
+  let data = {
+    name: name,
+    ingredients: [ingredient],
+    instructions: [instruction],
+  };
+
+  const foundRecipe = Recipes.findIndex((element) => element.name === name);
+  if (foundRecipe !== -1) {
+    Recipes[foundRecipe].instructions.push(instruction);
+    Recipes[foundRecipe].ingredients.push(ingredient);
+   let checkRecipe = Recipes.find((element) => element.name === name)
+    res.json({ result: checkRecipe });
+  } else {
+    Recipes.push(data);
+    let newRecipe =  Recipes.find((element) => element.name === data.name)
+    res.json({ result: newRecipe });
+  }
+});
+
+router.get("/recipe/:food", (req, res) => {
+  const { food } = req.params;
+  const foundRecipe = Recipes.find((element) => element.name === food);
+  if (foundRecipe) {
+    //console.log(foundUserParams)
+    res.json({ result: foundRecipe });
+  } else {
+    res.json({ result: "Recipe not found" });
+  }
 });
 
 module.exports = router;
