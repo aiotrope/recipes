977862741997b02494cffb7f5fe2dff43a574f53
aiotrope <arneli74@protonmail.com>
2023-06-09 19:39:10 +0300

    ♻️ refactor(app): edit files

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
-const fileupload = require('express-fileupload')
+var fileupload = require('express-fileupload')
 
 const indexRouter = require('./routes/index')
-//const recipeRouter = require('./routes/recipe')
-//const imagesRouter = require('./routes/images')
 
 const app = express()
 
@@ -34,7 +32,6 @@ var Recipes = [
   },
 ]
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
@@ -53,22 +50,16 @@ app.use(cors())
 app.use(fileupload())
 
 app.use('/', indexRouter)
-//app.use('/recipe', recipeRouter)
-//app.use('/images', imagesRouter)
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
 })
 
-// error handler
 // eslint-disable-next-line no-unused-vars
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
   res.status(err.status || 500)
   res.render('error')
 })
