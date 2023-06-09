ae55c1b2bf20bf8dcdc8093b5124f190b1283166
aiotrope <arneli74@protonmail.com>
2023-06-09 15:38:04 +0300

    🐛 fix(app): init

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# recipes
-Fourth weekly exercise.
+# storehouse
+Fifth weekly exercise.
 
 ## CLI Commands
 
 ```bash
-# init node project with express generator
-$ express --view=ejs recipes
+# clone project
+$ git clone git@github.com:aiotrope/recipes.git
 
 # install dependencies
 $ npm install
@@ -18,5 +18,33 @@ $ npm run dev
 
 # lint & format code
 $ npm run format && npm run lint
+
+## mogodb
+# start a shell
+$ mongosh
+
+# show databases
+$ show dbs
+
+# current db
+$ db
+
+# create db
+$ use database_name
+
+# remove current db
+$ db.dropDatabase()
+
+# create collection
+$ db.createCollection("collection_name")
+
+# print all collections
+$ show collections
+
+# insert docs to collection
+$ db.collection_name.insert({objects})
+
+# show all collection docs
+$ db.collection_name.find()
 
 ```
--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
-var fileupload = require('express-fileupload')
+const helmet = require('helmet')
+const fileupload = require('express-fileupload')
+const mongoSanitize = require('express-mongo-sanitize')
+
+const dbConnection = require('./utils/db')
 
 const indexRouter = require('./routes/index')
 const recipeRouter = require('./routes/recipe')
@@ -43,6 +47,8 @@ app.set('Recipes', Recipes)
 app.set('partialObj', partialObj)
 app.locals.globRecipesVar = Recipes
 
+dbConnection()
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -50,7 +56,10 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/uploads', express.static('uploads'))
 app.use(cors())
+app.use(helmet())
 app.use(fileupload())
+app.use(require('sanitize').middleware)
+app.use(mongoSanitize())
 
 app.use('/', indexRouter)
 app.use('/recipe', recipeRouter)
